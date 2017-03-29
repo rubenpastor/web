cb63570ece774e1a335de3dd81ffdd622c0b1889
rubenpastor <rubenpastor@hotmail.com>
2017-03-29 10:09:10 +0100

    formulario
    nuevos formularios

--- HTML5_BOOTSTRAP/formulario/form_angular/app.js
+++ HTML5_BOOTSTRAP/formulario/form_angular/app.js
@@ -1,10 +1,32 @@
-angular.module("MyApp", [])
-.controller("MyController", function ($scope) {
-  $scope.nombre = "Ruben";
-  $scope.email = "ruben@ruben.com";
-  $scope.text = [];
+angular.module("MyApp", ["LocalStorageModule"])
+.controller("MyController", function ($scope, localStorageService) {
+  if(localStorageService.get("angular-todolist")){
+    $scope.text = localStorageService.get("angular-todolist");
+  }
+  else {
+    $scope.text = [];
+  }
 
+  /*$scope.nombre = "Ruben";
+  $scope.email = "ruben@ruben.com";
+  //$scope.text = [];
+  */
+/*
   $scope.enviar = function () {
     $scope.text.push($scope.nombre);//+ $scope.email;
+  }*/
+
+  $scope.$watchCollection('text',function(newValue, oldValue){
+    localStorageService.set("angular-todolist", $scope.text);
+  })
+
+  $scope.enviar = function () {
+    $scope.text.push($scope.datos);
+    $scope.datos = {};
   }
+
+  $scope.limpiar = function () {
+    $scope.text = [];
+  }
+
 });
